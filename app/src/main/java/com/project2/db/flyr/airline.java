14c96030d809f3c2454f3039d13980e4ffe85614
muthukumaranR <mr0051@uah.edu>
2015-10-06 02:21:40 +0800

    authentication

---
+++ app/src/main/java/com/project2/db/flyr/airline.java
@@ -0,0 +1,30 @@
+package com.project2.db.flyr;
+
+/**
+ * Created by karthick on 06/10/2015.
+ */
+public class airline {
+    int id;
+    String username,password;
+    public void setid(int id){
+        this.id = id;
+
+    }
+    public int getid(){
+        return this.id;
+    }
+    public void setusername(String username){
+        this.username = username;
+
+    }
+    public String getusername(){
+        return this.username;
+    }
+    public void setpassword(String password){
+        this.password = password;
+
+    }
+    public String getpassword(){
+        return this.password;
+    }
+}
